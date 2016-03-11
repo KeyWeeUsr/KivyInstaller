04dab41ace298814b234f74c878b9c10194d7d6f
Peter Badida <KeyWeeUsr@users.noreply.github.com>
2016-03-11 20:04:36 +0100

    Version 1.1

--- README.md
+++ README.md
@@ -1,25 +1,30 @@
 # KivyInstaller
 ### Summary
-Everyone can mess things up even with simple installation or update. In version `1.8.0` there was a batch file for making things easier for beginners or regular users, but this file disappeared as the wheel came in. KivyInstaller is inspired by this `kivy.bat` file from old portable package with multiple extended functions:
-- install python2/python3 with kivy stable or master
-- update kivy stable, master or change version(stable<-->master)
-- uninstall kivy with python
-- all needed versions for debugging at one place
+Everyone can mess things up even with simple installation or update. In version `1.8.0` there was a batch file for making things easier for beginners or regular users, but this file disappeared as the wheel came in. KivyInstaller is inspired by this `kivy.bat` file from old portable package with multiple new functions:
+- Install python2/python3 with kivy stable or master.
+- Update kivy stable, master or change version (stable<-->master).
+- Update itself (in case of new features, bug fixes).
+- Uninstall kivy and python.
+- All needed versions for debugging at one place.
 
 ###### This batch isn't for compiling!
-Batch works with `pip` and builds from `.whl` files. KivyInstaller supports Windows 32bit and 64bit, you can easily download version you need. However, there's one catch. MSI Installers have a `Product code` for various reasons, for us it means you can install only one python with same build version x.x.B(the third number). Changing `Product code` in `.msi` file and running it again is the solution. Workaround seems to be using lower/higher build version, ie if you installed `2.7.11`, the another version would be `2.7.10` or `2.7.12`. You have to change build version in `kivy.bat`
-`.msi` doesn't affect having python2 and python3 at the same time in separate folders.
+Batch works with `pip` and builds from `.whl` files. KivyInstaller supports Windows 32bit and 64bit, you can easily download version you need. However, there's one catch. MSI Installers have a `Product code` for various reasons, for us it means you can install only one python with same build version x.x.B(the third number).
+
+You can change `Product code` with some free tools and install python manually with modified `.msi` however. Workaround also seems to be using lower/higher build version, i.e. if you installed `2.7.11`, the another version would be `2.7.10` or `2.7.12`. You have to change build version in `kivy.bat`
+
+`Product code` of msi doesn't affect having python2 and python3 at the same time in separate folders.
 
 Defaults: python versions `2.7.11` `3.4.4`, kivy-master `1.9.2`. Change them freely inside batch file if you need.
 ### How to use
 ##### Installation
 - Create a folder for your python and place `kivy.bat` inside.
+    - Alternatively, clone whole KivyInstaller repo with `git clone https://github.com/KeyWeeUsr/KivyInstaller`, python will be installed there.
 - Run `kivy.bat`, select your python and kivy versions, wait.
-    - To select y/n you have to type and press `enter`(`return`)
+    - To select y/n you have to type and press `enter`(`return`).
 - Enjoy!
 
 ##### Update
-Batch uses kivy wheels for installation, it means: for stable [pypi](https://pypi.python.org/pypi/Kivy/1.9.1) and for master [google drive](https://drive.google.com/folderview?id=0B1_HB9J8mZepOV81UHpDbmg5SWM&usp=sharing)
+Batch uses kivy wheels for installation, it means: for stable [pypi](https://pypi.python.org/pypi/Kivy/1.9.1) and for master [google drive](https://drive.google.com/folderview?id=0B1_HB9J8mZepOV81UHpDbmg5SWM&usp=sharing).
 
 ###### stable
 - `kivy update`
@@ -27,11 +32,26 @@ Batch uses kivy wheels for installation, it means: for stable [pypi](https://pyp
 ###### master
 - `kivy updatemaster`
 
+###### kivy.bat
+- `kivy batupdate`
+
+For more details use `kivy help` after successful installation.
+
+##### Install to existing python
+- Copy/clone `kivy.bat` to your folder with `python.exe` and run it.
+- Choose your installed python architecture version (32bit/64bit).
+- Select which python is installed (2.x/3.x).
+- Best to ignore registering extensions with `n` option, it won't work anyway because python is already installed.
+- Choose kivy version.
+- Enjoy!
+
 ##### Uninstall
 There's an option to remove python with kivy and other packages + cached pip files. However, this option works only if the whole path to folder with python is the same as used during installation - ie it relies on `.msi` python installer partially. You can delete msi after installation. If batch needs the msi, it'll download it.
 
-### Tip
-- Close `Touchtracer` example after installation with `Escape`, not with the "red X". X seems to prevent changing variable `first` in `kivy.bat` After successful installation variable `first` is equal to zero. If not, `kivy.bat` will act as installer once more.
+### Attention KivyInstaller v1.0 users!
+- Version 1.0 didn't have an option for self-updating, therefore `update_v1.bat` is available for manual updating to latest version which has this kind of updating implemented. Copy `update_v1.bat` inside `kivy.bat` folder and run it.
+- If anything goes wrong, there will be `update_v1.log` file available. After successful update you can remove it.
+- For anyone who would clone whole repo - updater will not run if your version isn't `==1.0`. If it annoys you, remove it.
 
 ### Problems
 If you experience problems, use `issues` in this repo.
--- kivy.bat
+++ kivy.bat
@@ -1,9 +1,9 @@
 ::Author: KeyWeeUsr @ https://github.com/KeyWeeUsr
-::Version: 1.0
+::Version: 1.1
 ::Inspired by kivy.bat file for kivy1.8.0
-::to reset file just change <first> to 1
-::bitsadmin is available since winXP SP2
-::if it is not available, download and install to C:\Windows
+::To reset file just delete "config.kivyinstaller"
+::Bitsadmin is available since winXP SP2
+::If it is not available, download and install to C:\Windows
 ::https://www.microsoft.com/en-us/download/details.aspx?id=18546
 @echo off
 set cp=0
@@ -19,8 +19,26 @@ set pyversion=0
 set gstreamer=0
 set master=1.9.2
 set installkivy=1
+set installerversion=1.1
 setlocal ENABLEDELAYEDEXPANSION
 set addlocal=ADDLOCAL^=DefaultFeature,PrivateCRT,TclTk,Documentation,Tools,Testsuite
+
+echo #####################################################################
+echo KivyInstaller v%installerversion%
+echo Author: KeyWeeUsr @ https://github.com/KeyWeeUsr
+echo.
+echo Report issues @ https://github.com/KeyWeeUsr/KivyInstaller/issues
+echo #####################################################################
+
+echo Looking for config file...
+if exist "%~dp0config.kivyinstaller" (
+    echo Config found, setting variables...
+    set first=0
+    for /f "delims=" %%z in ('type "%~dp0config.kivyinstaller"') do set %%z
+) else (
+    echo Config not found, running installation...
+    set first=1
+)
 
 if not %first%==1 (
     goto installed
@@ -32,9 +50,15 @@ if %processor_architecture%==AMD64 (
     )
 )
 if exist "%~dp0python.exe" (
-    echo Python is already installed.
-    goto uninstall
+    echo Python is already installed!
+    echo To continue with kivy installation choose "y"
+    echo To uninstall choose "n"
+    set /p kivycontinue="Continue with kivy installation? y/n"
+    if %kivycontinue%==n (
+        goto uninstall
+    )
 )
+
 :version
 set /p choice="Python version? 2/3"
 if %choice%==2 (
@@ -48,6 +72,7 @@ if %choice%==2 (
 ) else (
     goto version
 )
+
 :extensions
 set /p choice="Register python extensions (.py, .pyc, ...)? y/n"
 if %choice%==y (
@@ -57,6 +82,7 @@ if %choice%==y (
 ) else (
     goto extensions
 )
+
 :kivy
 set /p choice="Install Kivy? y/n"
 if %choice%==y (
@@ -75,19 +101,23 @@ set /p choice="Install gstreamer? y/n"
 if %choice%==y (
     set gstreamer=1
 )
+
 :nokivy
+if %kivycontinue%==y (
+    goto check
+)
 if %arch%==win32 (
     if exist "%~dp0py%pyversion%.msi" (
-        echo Installer is already downloaded
+        echo Installer is already downloaded!
     ) else (
-        echo Downloading Python installer
+        echo Downloading Python installer...
         bitsadmin.exe /transfer "GetPythonMSI" "https://www.python.org/ftp/python/%pyversion%/python-%pyversion%.msi" "%~dp0py%pyversion%.msi"
         echo Installing...
     )
     msiexec.exe /i "%~dp0py%pyversion%.msi" /qb /L*V "%~dp0msi.log" ALLUSERS=0 TARGETDIR="%~dp0" CURRENTDIRECTORY="%~dp0" %addlocal%
 ) else (
     if exist "%~dp0py%pyversion%.amd64.msi" (
-        echo Installer is already downloaded
+        echo Installer is already downloaded!
     ) else (
         bitsadmin.exe /transfer "GetPythonMSI" "https://www.python.org/ftp/python/%pyversion%/python-%pyversion%.amd64.msi" "%~dp0py%pyversion%.amd64.msi"
         echo Installing...
@@ -95,6 +125,7 @@ if %arch%==win32 (
     msiexec.exe /i "%~dp0py%pyversion%.amd64.msi" /qb /L*V "%~dp0msi.log" ALLUSERS=0 TARGETDIR="%~dp0" CURRENTDIRECTORY="%~dp0" %addlocal%
 )
 goto check
+
 :uninstall
 set /p choice="Uninstall? y/n"
 start python -c "f=open(r'%~dp0%0.bat');s=''.join(f.readlines()).replace('set first=0','set first=1');f.close();f=open(r'%~dp0%0.bat','w');f.write(s);f.close();"
@@ -114,10 +145,12 @@ if %arch%==win32 (
     )
 )
 if not exist "%~dp0python.exe" (
+    del /q "%~dp0config.kivyinstaller"
     rmdir /s /q "%~dp0Lib"
     rmdir /s /q "%~dp0Scripts"
     rmdir /s /q "%~dp0share"
     rmdir /s /q "%~dp0whls"
+    
 )
 set /p choice="Remove cached pip files? y/n"
 if %choice%==y (
@@ -128,16 +161,67 @@ if %choice%==y (
     )
 )
 goto end
+
+:batupdate
+echo Checking for updates...
+bitsadmin.exe /transfer "GetKivyInstaller" "https://raw.githubusercontent.com/KeyWeeUsr/KivyInstaller/master/kivy.bat" "%~dp0update_kivy.bat"
+set /a line=0
+for /f "delims=" %%l in ('type "%~dp0update_kivy.bat"') do (
+    set /a line=line+1
+    if !line! equ 2 (
+        set updateversion=%%l
+        set updateversion=!updateversion:~11!
+    )
+)
+if %installerversion% lss %updateversion% (
+    echo You are using KivyInstaller version %installerversion%!
+    echo New version %updateversion% is available!
+    if %onlycheck%==1 (
+        echo To install new version use - %~n0 batupdate
+        del "%~dp0update_kivy.bat"
+        goto console
+    )
+) else if %installerversion%==%updateversion% (
+    echo Your version is up to date!
+    del "%~dp0update_kivy.bat"
+    goto console
+)
+echo Backing up original file...
+xcopy "%~dp0kivy.bat" "%~dp0backup_kivy.bat"
+echo Updating...
+start cmd /c timeout /t 5 & xcopy "%~dp0update_kivy.bat" "%~dp0kivy.bat" & del %~dp0update_kivy.bat && cmd /c "%~dp0%~n0"
+exit
+
+:help
+echo.
+echo KivyInstaller v%installerversion%
+echo.
+echo Usage:
+echo   %~n0 ^<file^> [update] [updatemaster] [batcheck] [batupdate]
+echo          [uninstall] [help]
+echo.
+echo   ^<file^>             Run python(.py) file.
+echo   update             Update kivy wheel to the latest stable.
+echo   updatemaster       Update kivy wheel to the latest nightly-build.
+echo   batcheck           Check for new KivyInstaller version only.
+echo   batupdate          Check for new KivyInstaller version and update.
+echo   uninstall          Uninstall kivy, python and leave only %~n0.
+echo   help               Show this.
+echo.
+echo Optional:
+echo   idle               Python(.py) file editor with syntax highlighting
+exit
+
 :check
 if exist "%~dp0python.exe" (
-    echo Python is installed.
-    echo Installing pip
+    echo Python is installed!
+    echo Installing pip...
     python -m ensurepip
 )
 if %installkivy%==0 (
     goto end
 )
-echo Preparing Python for Kivy
+echo Preparing Python for Kivy...
 python -m pip install --upgrade pip wheel setuptools
 if %gstreamer%==1 (
     python -m pip install docutils pygments pypiwin32 requests wget kivy.deps.sdl2 kivy.deps.glew kivy.deps.gstreamer --extra-index-url https://kivy.org/downloads/packages/simple/
@@ -182,19 +266,34 @@ if %first%==0 (
     python -m pip install "%~dp0whls\Kivy-%master%.dev0-%cpwhl%-none-%arch%.whl"
 )
 del "%~dp0whls\Kivy-%master%.dev0-%cpwhl%-none-%arch%.whl"
+
 :kivyend
 del "%~dp0msi.log"
-echo Running touchtracer demo
+echo Close touchtracer demo with Escape key, do not use X!
+pause
+echo Running touchtracer demo...
 >"%~dp0temp.txt" python -c "exec(\"try:\n    import kivy;\nexcept ImportError:\n    print('unsuccessful');\")"
 find /c "unsuccessful" "%~dp0temp.txt"
 del "%~dp0temp.txt"
 if not %errorlevel%==1 (
     start python "%~dp0share\kivy-examples\demo\touchtracer\main.py"
-    start python -c "f=open(r'%~dp0%0.bat');s=''.join(f.readlines()).replace('set first=1','set first=0').replace('set arch=win32','set arch=%arch%').replace('set pyversion=0','set pyversion=%pyversion%').replace('set cp=0','set cp=%cp%').replace('set cpwhl=0','set cpwhl=%cpwhl%');f.close();f=open(r'%~dp0%0.bat','w');f.write(s);f.close();"
+    (echo cp=%cp%) > "%~dp0config.kivyinstaller"
+    (echo cpwhl=%cpwhl%) >> "%~dp0config.kivyinstaller"
+    (echo stable=%stable%) >> "%~dp0config.kivyinstaller"
+    (echo cp2=%cp2%) >> "%~dp0config.kivyinstaller"
+    (echo cp3=%cp3%) >> "%~dp0config.kivyinstaller"
+    (echo py3=%py3%) >> "%~dp0config.kivyinstaller"
+    (echo arch=%arch%) >> "%~dp0config.kivyinstaller"
+    (echo py2=%py2%) >> "%~dp0config.kivyinstaller"
+    (echo pyversion=%pyversion%) >> "%~dp0config.kivyinstaller"
+    (echo gstreamer=%gstreamer%) >> "%~dp0config.kivyinstaller"
+    (echo master=%master%) >> "%~dp0config.kivyinstaller"
+    (echo installkivy=%installkivy%) >> "%~dp0config.kivyinstaller"
 ) else (
-    echo Kivy was not installed properly
+    echo Kivy was not installed properly!
 )
 goto end
+
 :installed
 if [%1]==[update] (
     set stable=1
@@ -204,6 +303,13 @@ if [%1]==[update] (
     goto check
 ) else if [%1]==[uninstall] (
     goto uninstall
+) else if [%1]==[batcheck] (
+    set onlycheck=1
+    goto batupdate
+) else if [%1]==[batupdate] (
+    goto batupdate
+) else if [%1]==[help] (
+    goto help
 )
 set PATH=%~dp0;%~dp0Tools;%~dp0Lib\idlelib;%PATH%
 echo PATH:
@@ -215,6 +321,7 @@ echo Running "python.exe %*"
 python %*
 if not %errorlevel%==0 (pause)
 goto end
+
 :console
 echo.
 echo #####################################################################
@@ -228,6 +335,7 @@ del temp.txt
 python -c "exec(\"try:\n    import os, glob, re, datetime, sys;p=os.path.dirname(os.path.abspath(sys.executable));match = re.findall('(\d{8})', os.path.basename(max(glob.glob(p+'/whls/*.[Ww][Hh][Ll]*'), key=os.path.getctime))[:-4])[0];print(match[:2]+' '+datetime.date(1900, int(match[2:4]), 1).strftime('%%B')[:3]+' '+match[4:]);\nexcept:\n    pass\")" >temp.txt
 set /p wheel_version=<temp.txt
 del temp.txt
+echo - KivyInstaller: %installerversion%
 echo - Python: %python_version%
 echo - Kivy: %kivy_version%
 echo - Wheel: %wheel_version%
@@ -238,6 +346,7 @@ echo - Uninstall %~n0 uninstall
 echo #####################################################################
 echo.
 cmd
+
 :end
 pause
 exit
