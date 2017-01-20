db9c595b6435dce5df92d6162594093d02fa7b7f
Peter Badida <KeyWeeUsr@users.noreply.github.com>
2017-01-20 13:31:26 +0100

    Add more tests

---
+++ tests/test_install_admin_py2_32.bat
@@ -0,0 +1,12 @@
+set DEBUG=1
+set choice_privileges=y
+set choice_python=2
+set choice_ext=n
+set choice_kivy=y
+set choice_master=y
+set choice_gst=y
+set choice_dsgn=y
+set choice_shrt=y
+
+:: set 'arch32' with pipe to silence other 'set /p'
+(echo y)|kivy
--- tests/test_install_admin_py3_32.bat
+++ tests/test_install_admin_py3_32.bat
@@ -1,11 +1,12 @@
 set DEBUG=1
-set choice_privileges=n
-set choice_python=2
+set choice_privileges=y
+set choice_python=3
 set choice_ext=n
 set choice_kivy=y
 set choice_master=y
 set choice_gst=y
-set choice_shrt=n
+set choice_dsgn=y
+set choice_shrt=y
 
 :: set 'arch32' with pipe to silence other 'set /p'
 (echo y)|kivy
